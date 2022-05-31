a3d724bd76a5671c56872c2b60e9a7329b924b03
ErnstClement <75867922+ErnstClement@users.noreply.github.com>
2022-05-31 14:40:22 +0200

    code user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -12,10 +12,32 @@ exports.signup = (req, res, next) => {
       });
       user
         .save()
-        .then(() => res.status(201).json({ message: "utilisateur créé !" }))
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email }) // Récupération de l'utilisateur qui correspond à l'adresse Email
+    .then((user) => {
+      if (!user) {
+        // Si l'utilisateur n'existe pas
+        return res.status(401).json({ error: "Utilisateur introuvable !" }); // retourne un status 401 avec message d'erreur
+      }
+      bcrypt
+        .compare(req.body.password, user.password) // Comparaison du password avec l'outil Bcrypt entre le mot de passe rentré et le mot de passe haché
+        .then((valid) => {
+          if (!valid) {
+            // Si Mot de passe invalide
+            return res.status(401).json({ error: "Mot de passe incorrect" }); // retourne un status 401 avec message d'erreur
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: "TOKEN",
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
