09445ebd996bbc437d431b17fcce1e2fbc416d9f
ErnstClement <75867922+ErnstClement@users.noreply.github.com>
2022-06-03 00:49:14 +0200

    maj

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-  email: { type: String, require: true, unique: true },
+  email: {
+    type: String,
+    require: [true, "Veuillez entrer une adresse email"],
+    unique: true,
+  },
   password: { type: String, require: true },
 });
 
