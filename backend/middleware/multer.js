ec454c77df96937b786c9950092ccbde0470432a
ErnstClement <75867922+ErnstClement@users.noreply.github.com>
2022-06-06 18:01:36 +0200

    ajout du multer

---
+++ backend/middleware/multer.js
@@ -0,0 +1,33 @@
+// Importation de multer pour gerer les nouveaux fichiers
+const multer = require("multer");
+
+// Type de fichiers autorisé
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
+
+// Création de la destination ou multer va enrengistrer les images et les gerer ensuite
+const storage = multer.diskStorage({
+  // Destination d'enregistrement des images
+  destination: (req, file, callback) => {
+    // on fait un callback du dossier images vers le backend
+    callback(null, "images");
+  },
+  // Définition du nom de fichier à utiliser
+  filename: (req, file, callback) => {
+    // Création d'un nouveau nom de fichier avec un split pour remplacer les espaces par des underscores
+    let name = file.originalname.split(" ").join("_");
+    let extension = MIME_TYPES[file.mimetype];
+    name = name.replace("." + extension, "_");
+
+    // Callback et création du filename, ajout d'une date et ajout de l'extension avec les mimetypes paramétré plus haut
+    callback(null, name + Date.now() + "." + extension);
+  },
+});
+
+// Exportation du module, on insère "storage", et on ajoute une methode single image pour lui definir que c'est une image Unique
+module.exports = multer({
+  storage: storage,
+}).single("image");
