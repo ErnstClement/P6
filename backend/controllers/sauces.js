8db951f7b840ccb1b1fc16982e21be91461b34d8
ErnstClement <75867922+ErnstClement@users.noreply.github.com>
2022-06-29 00:18:27 +0200

    ajout des routes modify et like

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,6 +1,9 @@
 // Récupération du modèle sauce
 const Sauce = require("../models/sauces");
 
+// Récupération du module file-system pour faciliter les modifications sur nos sauces
+var fs = require("fs");
+
 //----création d'une nouvelle sauce-----------------
 exports.createSauce = (req, res, next) => {
   //On récupère les données envoyé par le frontend
@@ -91,11 +94,194 @@ exports.deleteSauce = (req, res, next) => {
           );
       });
     })
-    .catch((error) =>
-      res.status(500).json({
-        error,
-      })
+    .catch(
+      (error) =>
+        res.status(500).json({
+          error,
+        }),
+      console.log(res)
     );
 };
 console.log(Sauce);
 //-------------------------------
+
+//---Modifications des sauces--------
+
+exports.modifySauce = (req, res, next) => {
+  let sauceObject = {};
+  req.file
+    ? // Si la modification contient une image => Utilisation de l'opérateur ternaire comme structure conditionnelle.
+      (Sauce.findOne({
+        _id: req.params.id,
+      }).then((sauce) => {
+        // On supprime l'ancienne image du serveur
+        const filename = sauce.imageUrl.split("/images/")[1];
+        fs.unlinkSync(`images/${filename}`);
+      }),
+      (sauceObject = {
+        // On modifie les données et on ajoute la nouvelle image
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }))
+    : // Opérateur ternaire équivalent à if() {} else {} => condition ? Instruction si vrai : Instruction si faux
+      // Si la modification ne contient pas de nouvelle image
+      (sauceObject = {
+        ...req.body,
+      });
+  Sauce.updateOne(
+    // On applique les paramètre de sauceObject
+    {
+      _id: req.params.id,
+    },
+    {
+      ...sauceObject,
+      _id: req.params.id,
+    }
+  )
+    .then(() =>
+      res.status(200).json({
+        message: "Sauce modifiée !",
+      })
+    )
+    .catch((error) =>
+      res.status(400).json({
+        error,
+      })
+    );
+};
+//------------------------------
+
+//---Ajout/Suppression des likes-----
+
+exports.likeDislike = (req, res, next) => {
+  // Pour la route READ = Ajout/suppression d'un like / dislike à une sauce
+  // Like présent dans le body
+  let like = req.body.like;
+  // On prend le userID
+  let userId = req.body.userId;
+  // On prend l'id de la sauce
+  let sauceId = req.params.id;
+
+  if (like === 1) {
+    // Si il s'agit d'un like
+    Sauce.updateOne(
+      {
+        _id: sauceId,
+      },
+      {
+        // On push l'utilisateur et on incrémente le compteur de 1
+        $push: {
+          usersLiked: userId,
+        },
+        $inc: {
+          likes: +1,
+        }, // On incrémente de 1
+      }
+    )
+      .then(() =>
+        res.status(200).json({
+          message: "j'aime ajouté !",
+        })
+      )
+      .catch((error) =>
+        res.status(400).json({
+          error,
+        })
+      );
+  }
+  if (like === -1) {
+    Sauce.updateOne(
+      // S'il s'agit d'un dislike
+      {
+        _id: sauceId,
+      },
+      {
+        $push: {
+          usersDisliked: userId,
+        },
+        $inc: {
+          dislikes: +1,
+        }, // On incrémente de 1
+      }
+    )
+      .then(() => {
+        res.status(200).json({
+          message: "Dislike ajouté !",
+        });
+      })
+      .catch((error) =>
+        res.status(400).json({
+          error,
+        })
+      );
+  }
+  if (like === 0) {
+    // Si il s'agit d'annuler un like ou un dislike
+    Sauce.findOne({
+      _id: sauceId,
+    })
+      .then((sauce) => {
+        if (sauce.usersLiked.includes(userId)) {
+          // Si il s'agit d'annuler un like
+          Sauce.updateOne(
+            {
+              _id: sauceId,
+            },
+            {
+              $pull: {
+                usersLiked: userId,
+              },
+              $inc: {
+                likes: -1,
+              }, // On incrémente de -1
+            }
+          )
+            .then(() =>
+              res.status(200).json({
+                message: "Like retiré !",
+              })
+            )
+            .catch((error) =>
+              res.status(400).json({
+                error,
+              })
+            );
+        }
+        if (sauce.usersDisliked.includes(userId)) {
+          // Si il s'agit d'annuler un dislike
+          Sauce.updateOne(
+            {
+              _id: sauceId,
+            },
+            {
+              $pull: {
+                usersDisliked: userId,
+              },
+              $inc: {
+                dislikes: -1,
+              }, // On incrémente de -1
+            }
+          )
+            .then(() =>
+              res.status(200).json({
+                message: "Dislike retiré !",
+              })
+            )
+            .catch((error) =>
+              res.status(400).json({
+                error,
+              })
+            );
+        }
+      })
+      .catch((error) =>
+        res.status(404).json({
+          error,
+        })
+      );
+  }
+};
+
+//----------------------------------------------------------------
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -7,8 +7,12 @@ const stuffCtrl = require("../controllers/sauces");
 router.post("/", authentification, multer, stuffCtrl.createSauce);
 router.get("/", authentification, multer, stuffCtrl.getAllSauce);
 router.get("/:id", authentification, multer, stuffCtrl.getOneSauce);
+
 // Route qui permet de supprimer "une sauce"
 // Supprime la sauce avec l'ID fourni.
 router.delete("/:id", stuffCtrl.deleteSauce);
+
+router.put("/:id", authentification, multer, stuffCtrl.modifySauce);
+router.post("/:id/like", authentification, stuffCtrl.likeDislike);
 
 module.exports = router;
